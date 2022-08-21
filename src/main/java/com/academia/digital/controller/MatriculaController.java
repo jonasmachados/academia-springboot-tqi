aef29b10e34f494c6b4443e2f4787ef1f891dc62
jonasmachados <jonasmachado.ti@gmail.com>
2022-08-21 19:41:43 -0300

    create project

---
+++ src/main/java/com/academia/digital/controller/MatriculaController.java
@@ -0,0 +1,30 @@
+package com.academia.digital.controller;
+
+import com.academia.digital.entity.Matricula;
+import com.academia.digital.entity.form.MatriculaForm;
+import com.academia.digital.service.impl.MatriculaServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/matriculas")
+public class MatriculaController {
+
+  @Autowired
+  private MatriculaServiceImpl service;
+
+  @PostMapping
+  public Matricula create(@Valid @RequestBody MatriculaForm form) {
+    return service.create(form);
+  }
+
+  @GetMapping
+  public List<Matricula> getAll(@RequestParam(value = "bairro", required = false) String bairro) {
+    return service.getAll(bairro);
+  }
+
+}
+
---
+++ src/main/java/com/academia/digital/entity/form/AvaliacaoFisicaUpdateForm.java
@@ -0,0 +1,8 @@
+package com.academia.digital.entity.form;
+
+public class AvaliacaoFisicaUpdateForm {
+
+  private double peso;
+
+  private double altura;
+}
